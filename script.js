e127450d90d96a8ce077e8e1facd102dfeae3444
arisachue <arisachue@gmail.com>
2022-06-17 16:38:15 -0700

    Remove unneeded code/comments, add in comments

--- script.js
+++ script.js
@@ -1,48 +1,31 @@
-
- 
-// Example image tag
-// <img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.title}" title="${movie.title}"/>
-
-
-const imageBaseUrl = "https://image.tmdb.org/t/p/w400"
+// HTML DOM elements
 const movieGridElem = document.querySelector(".movie-grid")
 const searchEngine = document.querySelector("#search-input")
 const mainWebsite = document.querySelector(".popup")
 const ratings = document.querySelectorAll(".movie-votes")
 const webtitle = document.querySelector(".now-playing")
+
+// global variables 
+const imageBaseUrl = "https://image.tmdb.org/t/p/w400"
 const myApi = "e8308ebddbe7e86e8a3e6b1c2cb2b96e"
 var apiMovies = new Array()
 var searchedMovies = new Array()
 var loadingSearches = false;
 var lastSearchedWord = ""
-
 var curMovies = new Array()
-// var curIndex = 0;
-const LOADSIZE = 10;
 
-// function updateCurrent(num) {
-//     for (let i = curIndex; i < curIndex + num; i++) {
-//         curMovies.push(movies[i])
-//     }
-//     curIndex += num
-// }
-
+// get first 20 movies from now playing movies using movie database api
 const getResults = async () => {
     try {
-        console.log("in api request")
         const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&page=1`)
         const data = await res.json()
-        // console.log(data)
-        // console.log(data.results)
         for (let i = 0; i < data.results.length; i++) {
-            // var movie_url = imageBaseUrl + data.results[i].poster_path
             const curMovie = {
                 id: data.results[i].id,
                 title: data.results[i].title,
                 posterPath: data.results[i].poster_path,
                 voteAverage: data.results[i].vote_average,
             }
-            // console.log(curMovie)
             apiMovies.push(curMovie)
         }
     } catch (error) {
@@ -50,9 +33,9 @@ const getResults = async () => {
     }
 }
 
-
+// add passed in movie onto screen and display it
 function addMovies(movie) {
-    console.log("add movie")
+    // pick rotten tomatoes icon based on voter rating
     var movie_url = imageBaseUrl + movie.posterPath
     var tomatoType = "fresh tomato"
     var tomatoPic = "fresh-tomato.png"
@@ -64,7 +47,7 @@ function addMovies(movie) {
         tomatoType = "certified tomato"
         tomatoPic = "certified-tomato.png"
     }
-    // console.log(movie_url)
+    // add movie onto screen
     movieGridElem.innerHTML += `
         <div class="movie-card" id="movie-${movie.id}" onclick="openPopup(${movie.id})">
             <div class="outer-flip">
@@ -81,15 +64,13 @@ function addMovies(movie) {
             </div>
             <h3 class="movie-name">${movie.title}</h3>
         </div>`
-    // const curRate = document.querySelector(`#movie-${movie.id}`)
-    // console.log(curRate)
-    // curRate.addEventListener("click", () => openPopup(movie_url))
 }
 
-
-
+// get another page of movies from movie database api and add it to the bottom of the page
 const loadMore = async () => {
+    // find which page to get from the movie database api
     var nextPage = (apiMovies.length / 20) + 1;
+    // get more movies from now playing movies sections
     if (!loadingSearches) {
         const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&page=${nextPage}`)
         const data = await res.json()
@@ -104,6 +85,7 @@ const loadMore = async () => {
             apiMovies.push(curMovie)
         }
     }
+    // get more movies from the searched results 
     else {
         const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${myApi}&query=${lastSearchedWord}&page=${nextPage}`)
         const data = await res.json()
@@ -118,25 +100,13 @@ const loadMore = async () => {
             searchedMovies.push(curMovie)
         }
     }
-    
-
-    
-
-    // var curIndex = curMovies.length
-    // for (let i = curIndex; i < curIndex + 5; i++) {
-    //     if (i >= movies.length) {
-    //         break;
-    //     }
-    //     addMovies(movies[i])
-    //     curMovies.push(movies[i])
-    // }
 }
 
+// get first 20 movies from word passed into search engine using movie database api
 const searchMovieResults = async (word) => {
+    // search results
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${myApi}&query=${word}&page=1`)
     const data = await res.json()
-    console.log(data.results)
-    // word = word.toUpperCase()
     var newResult = new Array()
     for (let i = 0; i < data.results.length; i++) {
         const curMovie = {
@@ -145,46 +115,47 @@ const searchMovieResults = async (word) => {
             posterPath: data.results[i].poster_path,
             voteAverage: data.results[i].vote_average,
         }
-        // console.log(curMovie)
         newResult.push(curMovie)
     }
     return newResult
-    // for (let i = 0; i < movies.length; i++) {
-    //     var curTitle = movies[i].title.toUpperCase()
-    //     if (curTitle.includes(word)) {
-    //         newResult.push(movies[i])
-    //     }
-    // }
-    // return newResult
 }
 
+// onclick function for searching a movie
 async function searchMovie() {
+    // if valid search is passed
     if (searchEngine.value != "") {
+        // get searched word
         webtitle.innerHTML = "Search Results"
         lastSearchedWord = searchEngine.value
         movieGridElem.innerHTML = ``
+        // add search results to screen
         searchedMovies = await searchMovieResults(searchEngine.value)
         searchedMovies.forEach(addMovies)
+        // reset search engine back to placeholder
         searchEngine.value = null
         searchEngine.placeholder = "Search Movie..."
         loadingSearches = true
     }
+    // if non-valid, go back to home screen
     else {
         backToStandard()
     }
 }
 
+// onclick function to smoothly scroll to the top of the page
 function scrollToTop() {
     console.log("test")
     window.scrollTo(0, 0);
 }
 
+// call search function whenever user hits enter key
 document.addEventListener("keyup", function(event) {
     if (event.code === 'Enter') {
         searchMovie()
     }
 })
 
+// goes back to home screen with first 20 pages showing
 function backToStandard() {
     webtitle.innerHTML = "Now Playing"
     searchedMovies = new Array()
@@ -193,44 +164,42 @@ function backToStandard() {
     loadingSearches = false
 }
 
+// close the popup window 
 function disappearPopup() {
     const popUpElement = document.querySelector(".popup")
     const popUpChildElement = document.querySelector(".popupChild")
+    // put in the correct transition
     popUpElement.classList.remove("fadeIn")
     popUpElement.classList.add("fadeOut")
+    // remove popup
     setTimeout(() => {
         popUpElement.removeChild(popUpChildElement)
         popUpElement.style.visibility = "hidden";
     }, 1000)
 }
 
+// onclick function to open the popup window
 const openPopup = async (curID) => {
-    const youtubeLink = "https://www.youtube.com/embed/"
-
-    console.log("clicked")
-    console.log(curID)
-    
+    // get movie summary
     const res = await fetch(`https://api.themoviedb.org/3/movie/${curID}?api_key=${myApi}`)
     const data = await res.json()
-    console.log(data)
+    // get movie trailer
+    const youtubeLink = "https://www.youtube.com/embed/"
     const resVid = await fetch(`https://api.themoviedb.org/3/movie/${curID}/videos?api_key=${myApi}`)
     const dataVid = await resVid.json()
-    console.log(dataVid.results)
-    var trailerKey = dataVid.results[0].key
-    console.log(trailerKey)
-    console.log(data.overview)
 
+    var trailerKey = dataVid.results[0].key
     var trailer_url = youtubeLink + trailerKey
     var trailer_summary = data.overview
 
+    // remove any unneeded transitions (called previously) and add correct transition
     const popUpElement = document.querySelector(".popup")
     if (popUpElement.classList.length > 1) {
         popUpElement.classList.remove("fadeOut")
     }
     popUpElement.classList.add("fadeIn")
 
-    // trailer_url = "https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" 
-    // trailer_summary = "test summary"
+    // add popup onto screen
     mainWebsite.innerHTML += `
         <div class="popupChild">
             <button id="close" type="button" class="btn btn-default" onclick="disappearPopup()">X</button>
@@ -242,23 +211,9 @@ const openPopup = async (curID) => {
     popUpElement.style.visibility = "visible";
 }
 
-
+// display first 20 now playing movies everytime window loads
 window.onload = async function () {
     await getResults()
-    console.log(apiMovies)
-    // for (let i = 0; i < 10; i++) {
-    //     curMovies.push(movies[i])
-    // }
-    // curMovies.forEach(addMovies)
+
     apiMovies.forEach(addMovies)
-
-
-    // for (let i = 0; i < ratings.length; i++) {
-    //     ratings[i].addEventListener('click', function(event){
-    //         openPopup();
-    //     });
-    // }
 }
-
-// updateCurrent(8)
-// curMovies.forEach(addMovies)
